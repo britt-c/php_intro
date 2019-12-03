68a676f016063ea8367e0a125c566a5af33b4499
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-12-02 20:27:24 -0800

    ohhhhh yeahhhhh!! :)

--- class_work.php
+++ class_work.php
@@ -3,8 +3,8 @@ echo "Enter a word: ";
 $input = rtrim(fgets(STDIN));
 $word = $input;
 echo "Your word is " . strlen($word) . " letters long!!\n";
-if (strlen($word) > 5) {
-echo strrev($word);
+if (strlen($word) >= 5) {
+echo strrev(substr($word, 0, 4));
 echo "\n";
 } else {
 echo $word;
