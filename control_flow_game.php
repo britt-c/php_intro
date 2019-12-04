2b950cbeb9ac888d8e2079f806ecb3afef38a420
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-12-03 23:11:30 -0800

    cleaned some stuff up

--- control_flow_game.php
+++ control_flow_game.php
@@ -9,21 +9,20 @@ $your_number = rtrim(fgets(STDIN));
 echo "My number: " .  $my_number . "\n";
 echo "Your number: " . $your_number . "\n";
 
- if ($your_number > 9 || $your_number < 0) {
-  echo "Please type a number between 0 and 9\n";
-	exit;
- }
- 
+if ($your_number < 9 && $your_number > 0) {
  if ($my_number < $your_number) {
 	echo "You cheated!!\n";
 	echo "Pick again..\n";
+	exit;
  } elseif ($my_number == $your_number) {
 	echo "It's a tie!\n";
 	exit;
  } else {
 	echo "HAHA!! I won! You lose!\n";
-	echo "My number was " .  $my_number.to_s . "!!\n";
+	echo "My number was " .  $my_number . "!!\n";
 	exit;
  }
-
+} else { 
+ echo "Please enter a number between 0 and 9.\n";
+}
 ?>
