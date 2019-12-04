e8c572fe5048089d98056de2baf48b56bf0f0411
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-12-04 00:28:16 -0800

    added some constraints...still playing around

--- control_flow_game.php
+++ control_flow_game.php
@@ -11,22 +11,25 @@ while($winner != true) {
  $your_number = rtrim(fgets(STDIN));
  echo "My number: " .  $my_number . "\n";
  echo "Your number: " . $your_number . "\n";
-
+ if (is_numeric($your_number) != true) {
+  echo "Invalid entry...\n";
+ }
  if ($your_number < 9 && $your_number > 0) {
-  if ($my_number < $your_number) {
+	if ($my_number < $your_number) {
 	 echo "You cheated!!\n";
 	 echo "Pick again..\n";
 	 $winner = false;
   } elseif ($my_number == $your_number) {
 	 echo "It's a tie!\n";
-	 exit;
+	 $winner = true;
   } else {
 	 echo "HAHA!! I won! You lose!\n";
 	 echo "My number was " .  $my_number . "!!\n";
 	 exit;
   } 
  } else { 
-  echo "Please enter a number between 0 and 9.\n";
- }
+    echo "Please enter a NUMBER between 0 and 9.";
+	  echo "(e.g., 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)\n";
+  }
 }
 ?>
