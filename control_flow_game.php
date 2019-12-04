769f1d42c50d5cfc83b9a46462b070fd20bb92e7
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-12-03 22:53:37 -0800

    Making me loopy...

--- control_flow_game.php
+++ control_flow_game.php
@@ -11,15 +11,19 @@ echo "Your number: " . $your_number . "\n";
 
  if ($your_number > 9 || $your_number < 0) {
   echo "Please type a number between 0 and 9\n";
+	exit;
  }
- if ($my_number == $your_number) {
+ 
+ if ($my_number < $your_number) {
+	echo "You cheated!!\n";
+	echo "Pick again..\n";
+ } elseif ($my_number == $your_number) {
 	echo "It's a tie!\n";
- } elseif ($my_number < $your_number) {
-  echo "You cheated!\n";
-  echo "Pick again..\n";
+	exit;
  } else {
 	echo "HAHA!! I won! You lose!\n";
-	echo "My number was " +  my_number.to_s + "!!\n";
+	echo "My number was " .  $my_number.to_s . "!!\n";
+	exit;
  }
 
 ?>
