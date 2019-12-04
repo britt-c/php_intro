a39721e6bce9d78a24c1ff18403e8836819cbf35
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-12-04 01:42:45 -0800

    Debugged somestuff... still need to clean up a little

--- control_flow_game.php
+++ control_flow_game.php
@@ -11,25 +11,24 @@ while($winner != true) {
  $your_number = rtrim(fgets(STDIN));
  echo "My number: " .  $my_number . "\n";
  echo "Your number: " . $your_number . "\n";
- if (is_numeric($your_number) != true) {
-  echo "Invalid entry...\n";
- }
- if ($your_number < 9 && $your_number > 0) {
+ if (is_numeric($your_number) != true){
+	echo "INVALID ENTRY\n";
+ } elseif ($your_number <= 9 && $your_number >= 0) {
 	if ($my_number < $your_number) {
 	 echo "You cheated!!\n";
 	 echo "Pick again..\n";
-	 $winner = false;
   } elseif ($my_number == $your_number) {
 	 echo "It's a tie!\n";
-	 $winner = true;
-  } else {
-	 echo "HAHA!! I won! You lose!\n";
-	 echo "My number was " .  $my_number . "!!\n";
-	 exit;
-  } 
- } else { 
-    echo "Please enter a NUMBER between 0 and 9.";
-	  echo "(e.g., 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)\n";
-  }
+	 $winner = true; 
+	} elseif ($my_number > $your_number) {
+		echo "I win because my number was " . $my_number . "!\n";
+		echo "And your number was " . $your_number . "!\n";
+		echo "And in this game...bigger is better.\n";
+	  exit;
+	}
+ } else {
+		echo "Choose from the following #'s ONLY!!!\n";
+		echo "[1, 2, 3, 4, 5, 6,7, 8, 9, 0]\n";
+ }
 }
 ?>
